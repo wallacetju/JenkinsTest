4f6f80b851f391b9e3b6c79464722538e3842aff
shuai sun <wallacetju@gmail.com>
2014-12-19 00:09:08 -0500

    Remove all java code

--- src/TestJunit.java
+++
@@ -1,9 +0,0 @@
-import org.junit.Test;
-import static org.junit.Assert.assertEquals;
-public class TestJunit {
-   @Test
-   public void testAdd() {
-      String str= "Junit is working fine";
-      assertEquals("Junit is working fine",str);
-   }
-}
